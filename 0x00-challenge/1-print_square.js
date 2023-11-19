73d5ea27b34a7faedbad054b4db5fa28cfe5f213
Lebohang1821 <millboylebohang@gmail.com>
2023-11-19 13:31:23 -0800

    task-1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,21 +7,19 @@
     Example: ./print_square.js 8
 */
 
-// It check if required command-line argument is provided
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./print_square.js <size>\n");
-    process.stderr.write("Example: ./print_square.js 8\n");
-    process.exit(1);
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
 }
 
-// Parse size from command-line argument
-const size = parseInt(process.argv[2], 10);
+size = parseInt(process.argv[2], 10)
 
 for (let x = 0 ; x < size ; x ++) {
-	for (let l = 0 ; l < size ; l ++) {
-// It prints character # for each column
+    for (let l = 0 ; l < size ; l ++) {
         process.stdout.write("#");
     }
-// Move to the next line after printing a complete row
     process.stdout.write("\n");
+}
