6437af37b17c4a1b39077732ba3c4a1d2b0f6f77
Lebohang1821 <millboylebohang@gmail.com>
2024-01-29 05:29:50 -0800

    task-0

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Module for square class"""
 
 
 class Square():
-    """ Square class """
+    """ The square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Instantiation of class """
+        """ The nstantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Area of square """
         return self.width * self.height
 
     def permiter_of_my_square(self):
@@ -26,7 +25,7 @@ class Square():
 
 
 if __name__ == "__main__":
-    """ Create a square object """
+    """ It creates square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
